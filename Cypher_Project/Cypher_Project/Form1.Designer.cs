063e7943fec03dd716300649618fde1bb964bb8e
galtrikh <81478303+galtrikh@users.noreply.github.com>
2021-04-28 16:09:29 +0300

    Add files via upload

---
+++ Cypher_Project/Cypher_Project/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Cypher_Project
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.cypher_button = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.help_button = new System.Windows.Forms.Button();
+			this.Path_textBox_c = new System.Windows.Forms.TextBox();
+			this.Path_textBox_dec = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.Key_textBox_dec = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.Key_textBox_c = new System.Windows.Forms.TextBox();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(6, 69);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(143, 17);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "Улучшенная шифровка";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(235, 95);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Дешифровать";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// cypher_button
+			// 
+			this.cypher_button.Location = new System.Drawing.Point(6, 106);
+			this.cypher_button.Name = "cypher_button";
+			this.cypher_button.Size = new System.Drawing.Size(100, 23);
+			this.cypher_button.TabIndex = 2;
+			this.cypher_button.Text = "Зашифровать";
+			this.cypher_button.UseVisualStyleBackColor = true;
+			this.cypher_button.Click += new System.EventHandler(this.cypher_button_Click);
+			// 
+			// button3
+			// 
+			this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.button3.FlatAppearance.BorderSize = 2;
+			this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+			this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+			this.button3.Location = new System.Drawing.Point(235, 41);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 22);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Обзор";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// help_button
+			// 
+			this.help_button.Location = new System.Drawing.Point(2, 2);
+			this.help_button.Name = "help_button";
+			this.help_button.Size = new System.Drawing.Size(75, 23);
+			this.help_button.TabIndex = 6;
+			this.help_button.Text = "Помощь";
+			this.help_button.UseVisualStyleBackColor = true;
+			this.help_button.Click += new System.EventHandler(this.help_button_Click);
+			// 
+			// Path_textBox_c
+			// 
+			this.Path_textBox_c.Location = new System.Drawing.Point(6, 43);
+			this.Path_textBox_c.Name = "Path_textBox_c";
+			this.Path_textBox_c.Size = new System.Drawing.Size(220, 20);
+			this.Path_textBox_c.TabIndex = 7;
+			// 
+			// Path_textBox_dec
+			// 
+			this.Path_textBox_dec.Location = new System.Drawing.Point(6, 47);
+			this.Path_textBox_dec.Name = "Path_textBox_dec";
+			this.Path_textBox_dec.Size = new System.Drawing.Size(220, 20);
+			this.Path_textBox_dec.TabIndex = 8;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.Key_textBox_dec);
+			this.groupBox1.Controls.Add(this.Path_textBox_dec);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 182);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(341, 154);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Дешифровка";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 81);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 13);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "Криптоключ";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(171, 13);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "Путь до зашифрованного файла";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(235, 45);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "Обзор";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// Key_textBox_dec
+			// 
+			this.Key_textBox_dec.Location = new System.Drawing.Point(6, 97);
+			this.Key_textBox_dec.Name = "Key_textBox_dec";
+			this.Key_textBox_dec.Size = new System.Drawing.Size(220, 20);
+			this.Key_textBox_dec.TabIndex = 9;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.Key_textBox_c);
+			this.groupBox2.Controls.Add(this.Path_textBox_c);
+			this.groupBox2.Controls.Add(this.cypher_button);
+			this.groupBox2.Controls.Add(this.button3);
+			this.groupBox2.Controls.Add(this.checkBox1);
+			this.groupBox2.Location = new System.Drawing.Point(12, 31);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(319, 145);
+			this.groupBox2.TabIndex = 0;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Шифровка";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(112, 92);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(68, 13);
+			this.label4.TabIndex = 12;
+			this.label4.Text = "Криптоключ";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 27);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(223, 13);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Путь до файла для шифровки(*.txt, *.doc(x))";
+			// 
+			// Key_textBox_c
+			// 
+			this.Key_textBox_c.Location = new System.Drawing.Point(112, 108);
+			this.Key_textBox_c.Name = "Key_textBox_c";
+			this.Key_textBox_c.ReadOnly = true;
+			this.Key_textBox_c.Size = new System.Drawing.Size(198, 20);
+			this.Key_textBox_c.TabIndex = 10;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(366, 343);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.help_button);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "Cypher";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button cypher_button;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button help_button;
+		private System.Windows.Forms.TextBox Path_textBox_c;
+		private System.Windows.Forms.TextBox Path_textBox_dec;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox Key_textBox_dec;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TextBox Key_textBox_c;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+	}
+}
+
---
+++ Cypher_Project/Cypher_Project/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cypher_Project
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public static char[] Alphabet = {
+			'A',
+			'B',
+			'C',
+			'D',
+			'E',
+			'F',
+			'G',
+			'H',
+			'I',
+			'J',
+			'K',
+			'L',
+			'M',
+			'N',
+			'O',
+			'P',
+			'Q',
+			'R',
+			'S',
+			'T',
+			'U',
+			'V',
+			'W',
+			'X',
+			'Y',
+			'Z',
+			'a',
+			'b',
+			'c',
+			'd',
+			'e',
+			'f',
+			'g',
+			'h',
+			'i',
+			'j',
+			'k',
+			'l',
+			'm',
+			'n',
+			'o',
+			'p',
+			'q',
+			'r',
+			's',
+			't',
+			'u',
+			'v',
+			'w',
+			'x',
+			'y',
+			'z',
+
+			'А',
+			'Б',
+			'В',
+			'Г',
+			'Д',
+			'Е',
+			'Ё',
+			'Ж',
+			'З',
+			'И',
+			'Й',
+			'К',
+			'Л',
+			'М',
+			'Н',
+			'О',
+			'П',
+			'Р',
+			'С',
+			'Т',
+			'У',
+			'Ф',
+			'Х',
+			'Ц',
+			'Ч',
+			'Ш',
+			'Щ',
+			'Ъ',
+			'Ы',
+			'Ь',
+			'Э',
+			'Ю',
+			'Я',
+			'а',
+			'б',
+			'в',
+			'г',
+			'д',
+			'е',
+			'ё',
+			'ж',
+			'з',
+			'и',
+			'й',
+			'к',
+			'л',
+			'м',
+			'н',
+			'о',
+			'п',
+			'р',
+			'с',
+			'т',
+			'у',
+			'ф',
+			'х',
+			'ц',
+			'ч',
+			'ш',
+			'щ',
+			'ъ',
+			'ы',
+			'ь',
+			'э',
+			'ю',
+			'я',
+
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'6',
+			'7',
+			'8',
+			'9',
+			'0',
+		};
+
+		public static string[] binaryCodes = {
+			"00000000",
+			"00000001",
+			"00000010",
+			"00000011",
+			"00000100",
+			"00000101",
+			"00000110",
+			"00000111",
+			"00001000",
+			"00001001",
+			"00001010",
+			"00001011",
+			"00001100",
+			"00001101",
+			"00001110",
+			"00001111",
+			"00010000",
+			"00010001",
+			"00010010",
+			"00010011",
+			"00010100",
+			"00010101",
+			"00010110",
+			"00010111",
+			"00011000",
+			"00011001",
+
+			"00011010",
+			"00011011",
+			"00011100",
+			"00011101",
+			"00011110",
+			"00011111",
+			"00100000",
+			"00100001",
+			"00100010",
+			"00100011",
+			"00100100",
+			"00100101",
+			"00100110",
+			"00100111",
+			"00101000",
+			"00101001",
+			"00101010",
+			"00101011",
+			"00101100",
+			"00101101",
+			"00101110",
+			"00101111",
+			"00110000",
+			"00110001",
+			"00110010",
+			"00110011",
+			
+			"00110100",
+			"00110101",
+			"00110110",
+			"00110111",
+			"00111000",
+			"00111001",
+			"00111010",
+			"00111011",
+			"00111100",
+			"00111101",
+			"00111110",
+			"00111111",
+			"01000000",
+			"01000001",
+			"01000010",
+			"01000011",
+			"01000100",
+			"01000101",
+			"01000110",
+			"01000111",
+			"01001000",
+			"01001001",
+			"01001010",
+			"01001011",
+			"01001100",
+			"01001101",
+			"01001110",
+			"01001111",
+			"01010000",
+			"01010001",
+			"01010010",
+			"01010011",
+			"01010100",
+
+			"01010101",
+			"01010110",
+			"01010111",
+			"01011000",
+			"01011001",
+			"01011010",
+			"01011011",
+			"01011100",
+			"01011101",
+			"01011110",
+			"01011111",
+			"01100000",
+			"01100001",
+			"01100010",
+			"01100011",
+			"01100100",
+			"01100101",
+			"01100110",
+			"01100111",
+			"01101000",
+			"01101001",
+			"01101010",
+			"01101011",
+			"01101100",
+			"01101101",
+			"01101110",
+			"01101111",
+			"01110000",
+			"01110001",
+			"01110010",
+			"01110011",
+			"01110100",
+			"01110101",
+
+			"01110110",
+			"01110111",
+			"01111000",
+			"01111001",
+			"01111010",
+			"01111011",
+			"01111100",
+			"01111101",
+			"01111110",
+			"01111111"
+		};
+
+		public static string CypheredBinaryEnum = "";
+		private void button3_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.FileName = "";
+			openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Word Документы (*.doc)|*.doc|Word Документы (*.docx)|*.docx";
+			openFileDialog1.ShowDialog();
+			Path_textBox_c.Text = openFileDialog1.FileName;
+			Key_textBox_c.Text = $"{File.ReadAllText(Path_textBox_c.Text).Length}";
+		}
+
+		private void cypher_button_Click(object sender, EventArgs e)
+		{
+			string TextFromFile = File.ReadAllText(Path_textBox_c.Text);
+
+			Form Form2 = new Form();
+			Form2.Text = "TEXT";
+			Form2.Show();
+			Label label1 = new Label();
+			label1.Location = new Point(10, 10);
+			Form2.Controls.Add(label1);
+			label1.AutoSize = true;
+			label1.Text = TextFromFile;
+
+			for (int i = 0; i < TextFromFile.Length; i++)
+			{
+				for (int j = 0; j < Alphabet.Length; j++)
+				{
+					if (TextFromFile[i] == Alphabet[j])
+					{
+						CypheredBinaryEnum += binaryCodes[j];
+					}
+				}
+				
+			}
+			label1.Text = CypheredBinaryEnum;
+		}
+
+		private void help_button_Click(object sender, EventArgs e)
+		{
+			Form Help_Form = new Form();
+			Label Help_label = new Label();
+			Help_Form.Text = "Помощь";
+			Help_label.Location = new Point(10, 10);
+			Help_Form.Controls.Add(Help_label);
+			Help_label.AutoSize = true;
+			Help_label.Text = File.ReadAllText("help.txt"); //Help text set's at here <<<
+			Help_Form.ShowIcon = false;
+			Help_Form.MinimumSize = new Size(200, 100);
+			Help_Form.MinimizeBox = false;
+			Help_Form.MaximizeBox = false;
+			Help_Form.Size = Help_label.Size + new Size(40, 60);
+			Help_Form.FormBorderStyle = FormBorderStyle.FixedDialog;
+			Help_Form.StartPosition = FormStartPosition.CenterParent;
+			Help_Form.ShowDialog();
+		}
+
+	}
+}
